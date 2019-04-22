8a168a4044a18b9b0e5accd4c82daff224ad0198
Kevin Truong <32530239+Kevin-Truong@users.noreply.github.com>
2019-04-22 00:33:37 -0700

    Search any city with API

--- source/MakeRequest.js
+++ source/MakeRequest.js
@@ -1,6 +1,7 @@
 "strict mode";
 
 // Do a CORS request to get Davis weather hourly forecast
+let allweather;
 
 // Create the XHR object.
 function createCORSRequest(method, url) {
@@ -10,32 +11,50 @@ function createCORSRequest(method, url) {
 }
 
 // Make the actual CORS request.
-function makeCorsRequest() {
+function makeCorsRequest(city) {
 
-    let url = "http://api.openweathermap.org/data/2.5/forecast/hourly?q=Davis,CA,US&units=imperial&APPID=1a859727d426d067799208b297566176"
+  let url = "http://api.openweathermap.org/data/2.5/forecast/hourly?q=" + city + ",US&units=imperial&APPID=1a859727d426d067799208b297566176"
+  console.log(city);
+  let xhr = createCORSRequest('GET', url);
 
-    let xhr = createCORSRequest('GET', url);
+  // checking if browser does CORS
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
 
-    // checking if browser does CORS
-    if (!xhr) {
-      alert('CORS not supported');
-      return;
-    }
+  // Load some functions into response handlers.
+  xhr.onload = function () {
+    let responseStr = xhr.responseText;  // get the JSON string 
+    //let object = JSON.parse(responseStr);  // turn it into an object
+    allweather = JSON.parse(responseStr);
+    console.log(JSON.stringify(allweather, undefined, 2));
+    //console.log(JSON.stringify(object, undefined, 2));  // print it out as a string, nicely formatted
+    setWeather();
+  };
 
-    // Load some functions into response handlers.
-    xhr.onload = function() {
-        let responseStr = xhr.responseText;  // get the JSON string 
-        let object = JSON.parse(responseStr);  // turn it into an object
-        console.log(JSON.stringify(object, undefined, 2));  // print it out as a string, nicely formatted
-    };
+  xhr.onerror = function () {
+    alert('Woops, there was an error making the request.');
+  };
 
-    xhr.onerror = function() {
-      alert('Woops, there was an error making the request.');
-    };
+  // Actually send request to server
+  xhr.send();
+}
 
-    // Actually send request to server
-    xhr.send();
+function setWeather() {
+  console.log(allweather.list[0].main.temp);
 }
 
 // run this code to make request when this script file gets executed 
-makeCorsRequest();
+function makeRequest() {
+  let city = document.getElementById("city").value;
+  document.write(city.replace(/\s/g, ''));
+
+  if (!city) {
+    city = "Davis,CA";
+  }
+
+  makeCorsRequest(city);
+}
+
+makeRequest();
